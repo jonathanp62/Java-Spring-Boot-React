ab6e326be2d5bd0ae5c47a55393d5d051aa9de40
Jonathan Parker <jonathanp62@gmail.com>
2025-09-22 16:36:21 -0400

    Got the person OK API working from the React client

--- server/src/main/java/net/jmp/spring/boot/react/person/PersonController.java
+++ server/src/main/java/net/jmp/spring/boot/react/person/PersonController.java
@@ -38,10 +38,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -49,6 +46,7 @@ import java.util.List;
 import java.util.Map;
 
 /// The person controller.
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
